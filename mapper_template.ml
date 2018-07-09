5871b52a2ca621d4651a2bededbec41d09721771
Loïc Peyrot <lo.peyrot@laposte.net>
2018-07-09 15:36:43 +0200

    Useless function.

--- mapper_template.ml
+++ mapper_template.ml
@@ -3,21 +3,6 @@ open Ast_helper
 open Asttypes
 open Parsetree
 
-(* Not only do we not need type annotations, we also don’t want the user to see
- * the solution. *)
-let rec get_rid_of_body = function
-  (* Argument type annotation. *)
-  | {pexp_loc = loc; pexp_desc = Pexp_fun (rec_flag, e,
-      {ppat_desc = Ppat_constraint (pattern, ty)}, body)} ->
-        Exp.fun_ ~loc rec_flag e pattern (get_rid_of_body body)
-  | {pexp_loc = loc; pexp_attributes = attrs;
-      pexp_desc = Pexp_constraint _} ->
-        Exp.constant ~loc ~attrs (
-          Pconst_string ("Replace this string by your implementation.", None))
-  | {pexp_loc = loc} ->
-      raise (Location.Error (
-        Location.error ~loc "Not a function or lacking type annotations."))
-
 let template_structure_mapper mapper s =
   let remove_type_annotations_and_body = function
     | {pstr_desc = Pstr_value (_,
