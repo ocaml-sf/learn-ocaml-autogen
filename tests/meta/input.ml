6dbd91f8343a74e790d8377312c9ed08c232d173
Loïc Peyrot <lo.peyrot@laposte.net>
2018-07-27 20:44:59 +0200

    Remove record-based definition of meta.json

--- tests/meta/input.ml
+++ tests/meta/input.ml
@@ -1,27 +1,6 @@
 open Test_lib
 open Report
 
-(*
-type meta = {
-  learnocaml_version : string;
-  kind : string;
-  stars : int;
-  title : string;
-  short_description : string;
-  identifier : string;
-  authors : string list
-}
-*)
-
-(*
-let meta = {
-  stars = 3;
-  title = "A trivial exercise";
-  short_description = "Identity function";
-  authors = ["Me", "me@myself.fr"]
-}
-*)
-
 let%meta stars = 3
 let%meta title = "Blimey! this is a really trivial exercise"
 let%meta short_description = "Identity function"
