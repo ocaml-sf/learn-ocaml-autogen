40d9407e7671f3744255fc33aa96e6b7005d6885
Loïc Peyrot <lo.peyrot@laposte.net>
2018-08-01 18:40:36 +0200

    Install bubblewrap on osx

--- .travis-autogen.sh
+++ .travis-autogen.sh
@@ -2,9 +2,21 @@
 
 . .travis-ocaml.sh
 
-sudo add-apt-repository -y ppa:ansible/bubblewrap
-sudo apt-get -y update
-sudo apt-get install -y bubblewrap
+install_bubblewrap_on_linux () {
+  sudo add-apt-repository -y ppa:ansible/bubblewrap
+  sudo apt-get -y update
+  sudo apt-get install -y bubblewrap
+}
+
+install_bubblewrap_on_osx () {
+  brew install ansible
+}
+
+case $TRAVIS_OS_NAME in
+  osx) opam_update_on_osx ;;
+  linux) opam_update_on_linux ;;
+esac
+
 opam install -y opam-devel
 sudo cp /home/travis/.opam/4.05.0/lib/opam-devel/* /usr/local/bin
 hash -r
